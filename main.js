8d95254aeee3fea3b2291ffd977bbeb337ce6e3e
take-iwiw <take.iwiw2222@gmail.com>
2020-08-12 19:49:49 +0900

    image list control for still

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const elemVideo0 = document.getElementById("video0");
 const elemVideo1 = document.getElementById("video1");
 const elemImg0 = document.getElementById("img0");
 const elemImg1 = document.getElementById("img1");
+const elemSelectImg0 = document.getElementById("selectImg0");
+const elemSelectImg1 = document.getElementById("selectImg1");
 const objTextFps = document.getElementById("textFps");
 const objTextFrameCnt = document.getElementById("textFrameCnt");
 const objTextFrameNum = document.getElementById("textFrameNum");
@@ -24,10 +26,14 @@ function setForVideoInput() {
 	elemVideo1.hidden=""
 	elemImg0.hidden="hidden";
 	elemImg1.hidden="hidden";
+	elemSelectImg0.hidden="hidden";
+	elemSelectImg1.hidden="hidden";
 	elemTextUrl0.multiple="";
 	elemTextUrl1.multiple="";
 	elemTextUrl0.accept="video/*";
 	elemTextUrl1.accept="video/*";
+	objTextFps.hidden="";
+	objButtonPlay.hidden="";
 	g_currentMode = "video";
 	document.getElementById("radioImageTypeVideo").checked="checked";
 }
@@ -36,10 +42,14 @@ function setForStillInput() {
 	elemVideo1.hidden="hidden"
 	elemImg0.hidden="";
 	elemImg1.hidden="";
+	elemSelectImg0.hidden="";
+	elemSelectImg1.hidden="";
 	elemTextUrl0.multiple="multiple";
 	elemTextUrl1.multiple="multiple";
 	elemTextUrl0.accept="image/*";
 	elemTextUrl1.accept="image/*";
+	objTextFps.hidden="hidden";
+	objButtonPlay.hidden="hidden";
 	g_currentMode = "stills";
 	document.getElementById("radioImageTypeStills").checked="checked";
 	g_stillIndex = 0;
@@ -51,6 +61,8 @@ function adjustVideoSize() {
 	elemVideo1.width = videoSize;
 	elemImg0.width = videoSize;
 	elemImg1.width = videoSize;
+	elemSelectImg0.style.width = videoSize + "px";
+	elemSelectImg1.style.width = videoSize + "px";
 }
 
 window.onload = function() {
@@ -87,23 +99,48 @@ elemVideo0.addEventListener("timeupdate", function() {
 
 /*** Still images ***/
 function displayStill(index) {
+	let filename = "";
 	if (index < g_stillList0.length) {
-		var fr0 = new FileReader();
+		let fr0 = new FileReader();
+		filename = g_stillList0[index].name;
 		fr0.readAsDataURL(g_stillList0[index]);
 		fr0.onload = function(e) {
-			var src = e.target.result;
-			elemImg0.src = src;
+			elemImg0.src = e.target.result;
 		}
 	}
-	if (index < g_stillList1.length) {
-		var fr1 = new FileReader();
-		fr1.readAsDataURL(g_stillList1[index]);
-		fr1.onload = function(e) {
-			var src = e.target.result;
-			elemImg0.src = src;
+	for (let i = 0; i < g_stillList1.length; i++) {
+		if (g_stillList1[i].name == filename) {
+			let fr1 = new FileReader();
+			fr1.readAsDataURL(g_stillList1[i]);
+			fr1.onload = function(e) {
+			elemImg1.src = e.target.result;
+			}
 		}
 	}
+	/* use the following if you want to display the same index */
+	// if (index < g_stillList1.length) {
+	// 	let fr1 = new FileReader();
+	// 	fr1.readAsDataURL(g_stillList1[index]);
+	// 	fr1.onload = function(e) {
+	// 		elemImg1.src = e.target.result;
+	// 	}
+	// }
+
+	elemSelectImg0.value = filename;
+	elemSelectImg1.value = filename;
 }
+
+elemSelectImg0.addEventListener("change", function() {
+	console.log(elemSelectImg0.value);
+	for (let i = 0; i < g_stillList0.length; i++) {
+		if (g_stillList0[i].name == elemSelectImg0.value) {
+			g_stillIndex = i;
+			displayStill(g_stillIndex);
+			break;
+		}
+	}
+}, false);
+
 
 /*** Controllers ***/
 function controlPlayPause() {
@@ -188,12 +225,22 @@ document.getElementsByName("radioGroupImageType").forEach(function(e) {
 elemTextUrl0.addEventListener("change", function() {
 	console.log(elemTextUrl0.files);
 	if (g_currentMode == "video") {
+		if (elemTextUrl0.files.length != 1) return;
 		console.log(elemTextUrl0.files[0].name)
-		elemVideo0.src = elemTextUrl0.files[0].name;
+		// elemVideo0.src = elemTextUrl0.files[0].name;
+		let fr0 = new FileReader();
+		fr0.readAsDataURL(elemTextUrl0.files[0]);
+		fr0.onload = function(e) {
+			elemVideo0.src = e.target.result;
+		}
 	} else {
 		g_stillList0 = [];
 		for (let i = 0; i < elemTextUrl0.files.length; i++) {
 			g_stillList0.push(elemTextUrl0.files[i]);
+			let option = document.createElement("option");
+			option.text = elemTextUrl0.files[i].name;
+			option.val = elemTextUrl0.files[i].name;
+			elemSelectImg0.appendChild(option);
 		}
 		displayStill(g_stillIndex);
 	}
@@ -202,12 +249,23 @@ elemTextUrl0.addEventListener("change", function() {
 elemTextUrl1.addEventListener("change", function() {
 	console.log(elemTextUrl1.files);
 	if (g_currentMode == "video") {
+		if (elemTextUrl1.files.length != 1) return;
 		console.log(elemTextUrl1.files[0].name)
-		elemVideo1.src = elemTextUrl1.files[0].name;
+		// elemVideo1.src = elemTextUrl1.files[0].name;
+		let fr1 = new FileReader();
+		fr1.readAsDataURL(elemTextUrl1.files[0]);
+		fr1.onload = function(e) {
+			elemVideo1.src = e.target.result;
+		}
 	} else {
 		g_stillList1 = [];
 		for (let i = 0; i < elemTextUrl1.files.length; i++) {
 			g_stillList1.push(elemTextUrl1.files[i]);
+			let option = document.createElement("option");
+			option.text = elemTextUrl1.files[i].name;
+			option.val = elemTextUrl1.files[i].name;
+			option.disabled = "disabled";
+			elemSelectImg1.appendChild(option);
 		}
 		displayStill(g_stillIndex);
 	}
--- utils_for_windows/convMov2Still.bat
+++ utils_for_windows/convMov2Still.bat
@@ -3,5 +3,6 @@ rem echo off
 %~d0 
 cd %~dp0
 mkdir "%~n1"
-ffmpeg.exe -i "%1" -f image2 %~n1/%%06d.jpg
+ffmpeg.exe -i "%1" -f image2 "%~n1/%%06d.jpg"
 
+pause
